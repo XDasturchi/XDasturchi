ee16ba1f9e5eb924964cfa2a09483ead35798ad3
XDasturchi <113162612+XDasturchi@users.noreply.github.com>
2022-09-09 11:15:26 +0500

    Update xdasturchi.php

--- xdasturchi.php
+++ xdasturchi.php
@@ -1,1 +1,51 @@
-qalesn
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <img src="https://logos-world.net/wp-content/uploads/2020/04/Apple-Logo.png" alt="">
+            <a href="">Store</a>
+            <a href="">Mac</a>
+            <a href="">iPad</a>
+            <a href="">iPhone</a>
+            <a href="">Watch</a>
+            <a href="">AirPoda</a>
+            <a href="">TV & Home</a>
+            <a href="">Only on Apple</a>
+            <a href="">Accessories</a>
+            <a href="">Support</a>
+        </div>
+    </nav>
+    <style>
+        body{
+            margin: 0%;
+            padding: 0%;
+        }
+        nav{
+            background-color: rgb(39, 38, 38);
+            padding: 0.5 %;
+        }
+        .navbar img{
+            width: 50px;
+        }
+        .navbar{
+            display: flex;
+            width: 900px;
+            margin-left: 20%;
+            align-items: center;
+            justify-content: space-around;
+            word-spacing: 5%;
+        }
+        .navbar a{
+            color: gray;
+            text-decoration: none;
+        }
+    </style>
+</body>
+</html>
